c4423beb4c3ea183d4f20eb495b5508604730d58
zhouyufan0568 <l>
2017-12-24 12:21:21 +0800

    not important

--- Assets/UI/Scripts/UIController.cs
+++ Assets/UI/Scripts/UIController.cs
@@ -5,26 +5,29 @@ using UnityEngine;
 public class UIController : MonoBehaviour {
 
 	public GameObject backpack;
-	private Animator backpackAnimator;
-	private CanvasGroup backpackCanvasGroup;
+	//private Animator backpackAnimator;
+	//private CanvasGroup backpackCanvasGroup;
 
 	// Use this for initialization
 	void Start () {
-		backpackAnimator = backpack.GetComponent<Animator> ();
-		backpackCanvasGroup = backpack.GetComponent<CanvasGroup> ();
+		//backpackAnimator = backpack.GetComponent<Animator> ();
+		//backpackCanvasGroup = backpack.GetComponent<CanvasGroup> ();
+		backpack.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Backpack")) {
-			backpackAnimator.SetTrigger ("Open");
-			backpackCanvasGroup.interactable=true;
-			backpackCanvasGroup.blocksRaycasts = true;
+			//backpackAnimator.SetTrigger ("Open");
+			//backpackCanvasGroup.interactable=true;
+			//backpackCanvasGroup.blocksRaycasts = true;
+			backpack.SetActive(true);
 		}
 		if(Input.GetButtonUp("Backpack")){
-			backpackAnimator.SetTrigger ("Close");
-			backpackCanvasGroup.interactable=false;
-			backpackCanvasGroup.blocksRaycasts = false;
+			//backpackAnimator.SetTrigger ("Close");
+			//backpackCanvasGroup.interactable=false;
+			//backpackCanvasGroup.blocksRaycasts = false;
+			backpack.SetActive(false);
 		}
 	}
 }
