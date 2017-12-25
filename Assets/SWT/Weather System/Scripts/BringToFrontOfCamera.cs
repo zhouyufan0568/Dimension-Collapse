373a3632dcaf43d0b9e5264fbad1e13a7fe77f1d
swt369 <343011953@qq.com>
2017-12-25 22:07:28 +0800

    Revert "Revert "Merge branch 'SWT'""
    This reverts commit 71ba78af1ad4836e3de14d50c7a11919073fc17f.

---
+++ Assets/SWT/Weather System/Scripts/BringToFrontOfCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace DimensionCollapse
+{
+    public class BringToFrontOfCamera : MonoBehaviour
+    {
+        public float minY = 5f;
+        public float offsetZ = 5f;
+
+        private Camera localCamera;
+
+        private void Start()
+        {
+            localCamera = PlayerManager.LocalPlayerInstance.GetComponentInChildren<Camera>();
+        }
+
+        private void Update()
+        {
+            Vector3 front = localCamera.transform.position + localCamera.transform.forward * offsetZ;
+            transform.position = new Vector3(front.x, Mathf.Max(front.y, minY), front.z);
+            transform.rotation = Quaternion.Euler(0, localCamera.transform.rotation.y, 0);
+        }
+    }
+}
