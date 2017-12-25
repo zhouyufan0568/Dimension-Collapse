d9103a28fa721d02505dd73f484d60a6e8b1426b
swt369 <343011953@qq.com>
2017-12-25 21:55:05 +0800

    Add Weather System.

--- Assets/Online/PlayerManager.cs
+++ Assets/Online/PlayerManager.cs
@@ -2,32 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace DimensionCollapse {
-    public class PlayerManager : Photon.PunBehaviour, IPunObservable {
-
-        #region IPunObservable implementation
-        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-        {
-            //throw new System.NotImplementedException ();
-        }
-        #endregion
+namespace DimensionCollapse
+{
+    public class PlayerManager : Photon.PunBehaviour, IPunObservable
+    {
 
         #region Public Variables
 
-        [Tooltip("The current Health of our player")]
+        [Tooltip("The current health of our player")]
         public float Health = 1f;
 
         [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
         public static GameObject LocalPlayerInstance;
 
+        [Tooltip("The current direction of our player")]
         public float Direction;
+
+        [Tooltip("The current equipbar of our player")]
+        public GameObject[] equipbar;
+
+        [Tooltip("The current equipbar of our player")]
+        public GameObject[] skillbar;
+
+        [Tooltip("The current equipbar of our player")]
+        public GameObject[] itembar;
+
+        public bool isAlive;
 
         #endregion
 
+        #region Private Variables
+
         private Health health;
 
-        void awake() {
+        #endregion
 
+        #region MonoBehaviour Messages
+
+        void Awake()
+        {
             // #Important
             // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
             if (photonView.isMine)
@@ -37,19 +50,31 @@ namespace DimensionCollapse {
         }
 
         // Use this for initialization
-        void Start() {
-			if (photonView.isMine) {
-				GameObject.Find ("UIManager").SendMessage ("SetTarget", this, SendMessageOptions.RequireReceiver);
-			}
+        void Start()
+        {
+            if (photonView.isMine)
+            {
+                GameObject.Find("UIManager").SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+            }
+            isAlive = true;
             Direction = transform.rotation.eulerAngles.y;
             health = transform.GetComponent<Health>();
         }
 
         // Update is called once per frame
-        void Update() {
+        void Update()
+        {
             Direction = transform.rotation.eulerAngles.y;
-            Health = health.health/health.maxHealth;
-    
-    }
+            Health = health.health / health.maxHealth;
+        }
+
+        #endregion
+
+        #region IPunObservable implementation
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            //throw new System.NotImplementedException ();
+        }
+        #endregion
     }
 }
